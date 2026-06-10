214a5448451235aebb6747a99ded08aa71a5c7e6
Hassan043 <hassanali81213@gmail.com>
2026-06-10 03:02:43 -0700

    Add PatientHealth SQL project placeholders

---
+++ database/VitalNexus.PatientHealth.Database/PostDeployment.sql
@@ -0,0 +1,5 @@
+-- Post-deployment script for VitalNexus.PatientHealth.Database
+-- Add reference data population or post-deployment checks here.
+-- Post-deployment script for VitalNexus.PatientHealth.Database
+-- Add data seeding, permissions, and post-deploy validations here.
+GO
